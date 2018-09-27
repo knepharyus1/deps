23fed20dd3a8bd860a7de2536d03a9e4f08321b8
taemon1337 <taemon1337@gmail.com>
2018-09-26 22:19:23 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 urlfile="./urls"
 datadir="./data"
+mkdir -p $datadir
 
 TARGET_BRANCH=${TARGET_BRANCH:-build}
 REPO=https://taemon1337:${GITHUB_TOKEN}@github.com/clustellar/deps.git
@@ -19,11 +20,13 @@ git_setup() {
 fetch_urls() {
   for url in $(cat $urlfile); do
     ofile=$(echo $url | md5sum | cut -d' ' -f1)
-    curl -k $url -o $datadir/$ofile
+    echo "[GET] $ofile - $url"
+    curl -kv $url -o $datadir/$ofile
   done
 }
 
 git_push() {
+  echo "[GIT] git push $TARGET_BRANCH"
   git add $datadir
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
   git push -u $NEWORIGIN $TARGET_BRANCH
