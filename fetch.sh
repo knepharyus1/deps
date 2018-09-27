9803c4093b2463b049712efe571b597b05650939
taemon1337 <taemon1337@gmail.com>
2018-09-26 21:36:30 -0400

    trying this

--- fetch.sh
+++ fetch.sh
@@ -3,8 +3,22 @@
 urlfile="./urls"
 datadir="./data"
 
-for url in $(cat $urlfile); do
-  ofile=$(echo $url | md5sum | cut -d' ' -f1)
-  curl -k $url -o $datadir/$ofile
-done
+fetch_urls() {
+  for url in $(cat $urlfile); do
+    ofile=$(echo $url | md5sum | cut -d' ' -f1)
+    curl -k $url -o $datadir/$ofile
+  done
+}
 
+git_push() {
+  TARGET_BRANCH=${TARGET_BRANCH:-build}
+  REPO=`git config remote.origin.url`
+  SSH_REPO=${REPO/git@github.com/${GITHUB_TOKEN}@github.com}
+  git config user.name "Travis CI"
+  git config user.email "$COMMIT_AUTHOR_EMAIL"
+  git push $SSH_REPO $TARGET_BRANCH
+}
+
+
+fetch_urls
+git_push
